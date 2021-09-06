9a14b5bfd7be5d5194e15ad49aebb78f00bcdbd3
Tom Hayton <t.hayton37@gmail.com>
2021-09-06 11:51:40 +0100

    fixed navbar svg scaling

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,8 +1,7 @@
 import React from 'react';
 import styles from 'src/styles/layout.module.scss'
 import { ReactComponent as Bars } from 'src/assets/svgs/bars.svg'
-import { ReactComponent as LogoSVG } from 'src/assets/svgs/react.svg'
-import Logo from 'src/assets/imgs/Logo.png'
+import { ReactComponent as Logo } from 'src/assets/svgs/react.svg'
 
 interface Props {};
 
@@ -11,9 +10,8 @@ function Navbar(props: Props) {
 
     return (
         <nav className={styles.navContainer}>
-            <div className={styles.itemVisibleWhenCollapsed}><Bars /></div>
-            <div className={styles.itemVisibleWhenCollapsed}><LogoSVG /></div>
-            {/* <div className={styles.itemVisibleWhenCollapsed}><img src={Logo} alt="" /></div> */}
+            <div className={`${styles.itemVisibleWhenCollapsed} ${styles.bars}`}><Bars x={0} y={0} viewBox="0 0 125 55"/></div>
+            <div className={`${styles.itemVisibleWhenCollapsed} ${""}`}><Logo x={0} y={0} viewBox="0 0 220 190"/></div>
         </nav>
     );
 };
