52165441b700093c1b27970ba334ff3c9aff8cb8
Tom Hayton <t.hayton37@gmail.com>
2021-09-12 11:00:31 +0100

    added loading states

--- src/components/BlogPreviewItem.tsx
+++ src/components/BlogPreviewItem.tsx
@@ -10,24 +10,32 @@ interface Props {
 
 function BlogPreviewItem(props: Props) {
     const {title, excerpt, meta, feautredMediaId} = props;
-    const [mediaUrl, setMediaUrl] = useState<string | null>(null)
+    const [mediaUrl, setMediaUrl] = useState<string | null>(null);
+    const [isLoading, setIsLoading] = useState<boolean>(true);
 
     useEffect(() => {
-        if(feautredMediaId === 0) return;
         fetch(`http://www.react-test.dev.cc/wp-json/wp/v2/media/${feautredMediaId}`)
         .then(res => res.json())
-        .then(res => setMediaUrl(res.media_details.sizes.full.source_url))
-        .catch(err => console.log(err))
+        .then(res => {
+            setMediaUrl(res.media_details.sizes.full.source_url);
+            // if(!window.sessionStorage.getItem("keto-bps")) window.sessionStorage.setItem("keto-bps", "");
+            setIsLoading(false);
+        })
+        .catch(err => {
+            console.log(err);
+            setIsLoading(false);
+        });
     }, []);
 
-    const pushBlogSlug = () => {
-        
-    }
+    // const populateSessionStorage = (root: string, res: ) => {
+    //     if(typeof window === "undefined") return
+    //     const fromStorage = window.sessionStorage.getItem("keto-bps")?.split(",") || [];
+    //   }
 
     return (
         <div className={styles.previewCard} onClick={() => {}}>
             <div className={`${styles.cardImgContainer} ${mediaUrl ? "" : styles.emptyImgContainer}`}>
-               { mediaUrl ? <img src={mediaUrl} alt="" /> : <p>No Image Found...</p>}
+               { mediaUrl ? <img src={mediaUrl} alt="" /> : <p>{isLoading ? "Loading Image..." : "No Image Found..."}</p>}
             </div>
             <h3>{title}</h3>
             <p dangerouslySetInnerHTML={{__html: excerpt}}></p>
--- src/pages/blogPage.tsx
+++ src/pages/blogPage.tsx
@@ -1,7 +1,8 @@
-import React, { useEffect, useMemo, useState } from 'react';
+import React, { ReactElement, useEffect, useMemo, useState } from 'react';
+import styles from 'src/styles/blog.module.scss';
 import PageContentColumns from '../components/PageContentColumns';
 import BlogPreviewItem from '../components/BlogPreviewItem';
-import { Route, Switch, useRouteMatch } from 'react-router';
+import { Switch, useRouteMatch } from 'react-router';
 import { Link } from 'react-router-dom';
 
 interface Props {};
@@ -9,34 +10,95 @@ interface Props {};
 function BlogPage(props: Props) {
     const {} = props;
     const [wpData, setWpData] = useState<any>(null);
-    const { url } = useRouteMatch();
+    const [isLoading, setIsLoading] = useState<boolean>(true);
+    const { url, path } = useRouteMatch();
+
+    type cacheMatch = {id: number, isStillValid: boolean};
+    type cacheUpdateResponse = {success: boolean, isUpdateRequired?: boolean, matchedPosts?: cacheMatch[], err?: any};
+    // TODO Configure to have response composed of success and fail
+    type cacheUpdateSuccess = {success: boolean, isUpdateRequired: boolean, matchedPosts: cacheMatch[]};
+    type cacheUpdateFail = {success: boolean, isUpdateRequired: boolean, err?: any};
 
     useEffect(() => {
+        // const cacheUpdateRes: cacheUpdateResponse = checkCacheUpdateRequiredWithDB();
+        // if(cacheUpdateRes.success && !cacheUpdateRes.isUpdateRequired) {
+        //     setIsLoading(false);
+        //     return;
+        // }
+        // const successfulCacheUpdateResponse = cacheUpdateRes as cacheUpdateSuccess;
+        // const cachePostsToUpdate = successfulCacheUpdateResponse.matchedPosts.filter((response: cacheMatch) => !response.isStillValid);
+        // const updateIds = cachePostsToUpdate.map(post => post.id);
+        // const updateIdsString = updateIds.toString();
+        // const updateIdQueryParam = updateIdsString.replace(",","/");
+        // console.log(updateIdQueryParam)
+
         fetch("http://www.react-test.dev.cc/wp-json/wp/v2/posts")
             .then( res => res.json())
-            .then(formattedRes => {setWpData(formattedRes); console.log(formattedRes)})
-            .catch(err => console.log(err));
-    }, []);
+            .then(formattedRes => {
+                setWpData(formattedRes); 
+                // console.log(formattedRes);
+                setIsLoading(false);
+                // const fromStorage = window.sessionStorage.getItem("keto-bps");
+                // const parsedStorage  = fromStorage ? JSON.parse(fromStorage) : [];
+
+                // const resString = JSON.stringify(formattedRes);
+
+                // if(parsedStorage.filter((storageObj: any) => formattedRes.includes(storageObj)).length) return;
+                // parsedStorage.push(resString);
+                // window.sessionStorage.setItem("keto-bps", parsedStorage.toString());
+            })
+            .catch(err =>{
+                console.log(err);
+                setIsLoading(false);
+            });
+        }, []);
+        
+    const checkCacheUpdateRequiredWithDB = (): cacheUpdateResponse => {
+        // Probe db to compare posts with any cached posts in the browser
+        // If a post hasn't been cached, or a cached post is outdated, trigger a full info fetch for that post
+        let status: cacheUpdateResponse = {success: false};
+        let isUpdateRequired = false;
+        fetch("http://www.react-test.dev.cc/wp-json/wp/v2/posts?_fields=id,modified")
+        .then(res => res.json())
+        .then(resJSON => {
+            const fromStorage = window.sessionStorage.getItem("keto-bps");
+            // TODO set this to have a "should update all" effect, or separate that logic into separate function
+            if(fromStorage === null) {status = {success: true}; return status};
+            const parsedStorage = JSON.parse(fromStorage);
+            const matchedPosts = parsedStorage?.map((storageObj: any) => ({id: storageObj.id, isStllValid: !!resJSON.find((resObj: {id: number, modified: string}) => {
+                const shouldUpdate = resObj.id === storageObj.id && resObj.modified === storageObj.modified;
+                if(shouldUpdate && !isUpdateRequired) isUpdateRequired = true;
+                return shouldUpdate
+            })}));
+            status = {success: true, matchedPosts, isUpdateRequired}
+        })
+        .catch(err => status = {success: false, err})
+        return status;
+    }
 
     const blogPosts = useMemo(() => {
         if(!wpData) return;
         const postArry = Array.from(wpData);
-        return postArry.map((post: any) => {
+        const blogPostArry = postArry.map((post: any) => {
             const { title, excerpt, modified, id, featured_media, slug}: {title: {rendered: string}, excerpt: {rendered: string, protected: boolean,}, modified: string, id: number, featured_media: number, slug: string} = post;
-            return <Link to={`${url}/${slug}`}><BlogPreviewItem title={title.rendered} excerpt={excerpt.rendered} meta={[modified]} key={`blog_post_prev_id:${id}`} feautredMediaId={featured_media}/></Link>;
+            return <Link to={`${url}/${slug}`} className={styles.previewCardContainer}><BlogPreviewItem title={title.rendered} excerpt={excerpt.rendered} meta={[modified]} key={`blog_post_prev_id:${id}`} feautredMediaId={featured_media}/></Link>;
         });
-
+        setIsLoading(false);
+        return blogPostArry;
     }, [wpData]);
 
+
+    const getRenderedElements = () => {
+        if(isLoading) return <p>Loading...</p>;
+        if(!blogPosts) return <p>An error occurred</p>;
+        if(!blogPosts.length) return <p>No blog posts found...</p>;
+        return blogPosts;
+    };
+
     return (
-        <>
-            <PageContentColumns title={"Blog"}>
-                <>
-                    {blogPosts}
-                </>
-               
-            </PageContentColumns>
-        </>
+        <PageContentColumns title={"Blog"}>
+            { getRenderedElements() }
+        </PageContentColumns>
     );
 };
 
