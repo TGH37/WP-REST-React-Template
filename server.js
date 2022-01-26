62e8a6c81a0103d3171e2dce027d4f1ca7c00488
Tom Hayton <t.hayton37@gmail.com>
2022-01-26 13:48:50 +0000

    updated build configs and express server added for deployment

---
+++ server.js
@@ -0,0 +1,18 @@
+import express from 'express'
+import path from 'path'
+
+const app = express();
+const PORT = process.env.PORT || 5000;
+
+if(process.env.NODE_ENV === 'production') {
+    app.use(express.static('dist'));
+    app.get('*', (req, res) => {
+        req.sendfile(path.resolve(__dirname, 'build', 'index.html'));
+    })
+}
+
+app.listen(PORT, () => {
+    console.log("App up and running on port: " + PORT);
+})
+
+// export {}
