84c77eb871c06fcb9a9839522bb1e5c6a8cb3620
Tom Hayton <t.hayton37@gmail.com>
2022-01-26 14:17:49 +0000

    changed req to res when sending file

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ const PORT = process.env.PORT || 5000;
 if(process.env.NODE_ENV === 'production') {
     app.use(express.static('dist'));
     app.get('*', (req, res) => {
-        req.sendFile(path.resolve(__dirname, 'build', 'index.html'));
+        res.sendFile(path.resolve(__dirname, 'build', 'index.html'));
     });
 };
 
